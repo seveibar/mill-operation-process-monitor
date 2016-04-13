91f6d56fc5dca901d898bdde65344bd3448919ec
Andrey Choi <choia3@rpi.edu>
2016-04-12 21:03:25 -0400

    Actually working version of the python gcode parser. Better comments will be added later with a readme

--- gcode_parser/python/gcode_parser.py
+++ gcode_parser/python/gcode_parser.py
@@ -4,71 +4,60 @@ from pprint import pprint
 import math
 import matplotlib.pyplot as plt
 
-def _initialization( curCommandInt, time, xPos, yPos, zPos, commandList, XYZ, timeXYZRecord):
-	#Finds the initial xyz coordinates
-	curCommandInt = 1
-	for i in range(1,3):
-		curCommandInt += 1
+def _initialization( curCommandInt, time, xPos, yPos, zPos, commandList, timeXYZRecord):
+	measurementUnits = ''
+	for i in range(1,4):
+		curCommandInt = i
 		if( (commandList[curCommandInt])[0] == 'X' ):
 			xPos = float( (commandList[curCommandInt])[1:] )
 		elif( (commandList[curCommandInt])[0] == 'Y' ):
 			yPos = float( (commandList[curCommandInt])[1:] )
 		else:
 			zPos = float( (commandList[curCommandInt])[1:] )
-
-	#Stores the initial xyz coordinates into a list
-	XYZ.append( xPos )
-	XYZ.append( yPos )
-	XYZ.append( zPos )
-	#Stores the xyz coordinates into a dictionary that has a key of the time
-	timeXYZRecord[time] = XYZ
-	#Returns the feed rate and the measurement units
+	timeXYZRecord[time] = [ xPos, yPos, zPos ]
+	
 	curCommandInt += 1
 	if( (commandList[curCommandInt]) == 'G20' ):
 		measurementUnits = "Inches"
 	else:
 		measurementUnits = "Millimeters"
-	for i in range(1,4):
+	
+	for i in range(0,3):
 		curCommandInt+=1
-	feedRateNumber = float((((commandList)[curCommandInt])[1:]))
-	curCommandInt+=1
-	return (feedRateNumber, measurementUnits, curCommandInt, xPos, yPos, zPos )
+	feedRateNumber = float(commandList[curCommandInt][1:]) / 60.0
+	curCommandInt += 1
+	return ( curCommandInt, xPos, yPos, zPos, feedRateNumber, measurementUnits )
 
-def _movementGCode( curCommandInt, time, xPos, yPos, zPos, feedRate, commandList, XYZ, timeXYZRecord):
+def _movementGCode( curCommandInt, time, xPos, yPos, zPos, feedRate, commandList, timeXYZRecord):
 	oldXPos = xPos
 	oldYPos = yPos
 	oldZPos = zPos
 	if( commandList[curCommandInt] == "G01" ):		#Movement G-Code
-		timeXYZRecord[time] = [ oldXPos, oldYPos, oldZPos ]
 		curCommandInt+=1
 		while( (commandList[curCommandInt])[0] == 'X' or (commandList[curCommandInt])[0] == 'Y' or (commandList[curCommandInt])[0] == 'Z' ): #Calculates the new position
 			if( (commandList[curCommandInt])[0] == 'X' ):	#If the movement is in the X axis
 				xPos += float((((commandList)[curCommandInt])[1:]))	#Gets the X position
-				XYZ[0] = xPos;
 			elif( (commandList[curCommandInt])[0] == 'Y' ):	#If the movement is in the Y axis
 				yPos += float((((commandList)[curCommandInt])[1:]))	#Gets the Y position
-				XYZ[1] = xPos;
 			else:	#If the movement is in the Z axis
 				zPos += float((((commandList)[curCommandInt])[1:]))	#Gets the Z position
-				XYZ[2] = xPos;
 			curCommandInt += 1
+			
 		#Calculates the distance using pythagorean theorem
 		disXSquare = float(xPos - oldXPos) * float(xPos - oldXPos)
 		disYSquare = float(yPos - oldYPos) * float(yPos - oldYPos)
 		disZSquare = float(zPos - oldZPos) * float(zPos - oldZPos)
 		totalDistance = float(math.sqrt( disXSquare + disYSquare + disZSquare ))
 		#Calculates the time taken to make the movement
-		time += totalDistance / feedRate		#Time of movement = distance/feed rate
-		timeXYZRecord[time] = XYZ		#Inserting in the recent movement
-
-		#Pause G-Code
+		time += (totalDistance / feedRate ) 		#Time of movement = distance/feed rate
+	
 	elif(  commandList[curCommandInt] == "G4" or commandList[curCommandInt] == "G04" ):
-		curCommandInt+=1
-		time += float((((commandList)[curCommandInt])[1:]))	#Adds the time from the pause
-		timeXYZRecord[time] = [ oldXPos, oldYPos, oldZPos ]		#Inserting the position after the pause
 		curCommandInt += 1
-	return ( curCommandInt, xPos, yPos, zPos, time )
-
+		time += float((((commandList)[curCommandInt])[1:]))	#Adds the time from the pause
+		curCommandInt += 1
+	timeXYZRecord[time] = [ xPos, yPos, zPos]		#Inserting in the recent movement/pause
+	
+	return ( curCommandInt, time, xPos, yPos, zPos )
 
 def parse(filename, output_file=None, graph=False):
 	#Values of the machine data at the most recent instruction
@@ -77,24 +66,20 @@ def parse(filename, output_file=None, graph=False):
 	yPos = 0.0
 	zPos = 0.0
 	time = 0.0
-
-	#Various data structures to hold the xyz values and the commands
-	commandList = list() #Used for holding all of the commands in the file
-	XYZ = list() #Used for holding the "current" coordinates of the machine
-	timeXYZRecord = dict() #Used for holding the xyz values at time t
-
-	#Values for the feed rate
+	timeXYZRecord = {}
+	
 	feedRate = 0.0
 	measurementUnits = ''
-
+	commandList = []
+	
 	#Stores all the commands and stuff into a list.......very inefficient
 	with open(filename,'r') as f:
 		for line in f:
 			for command in line.split():
 				commandList.append( command )
-
-	#Initializing the values
-	feedRate, measurementUnits, curCommandInt, xPos, yPos, zPos = _initialization( curCommandInt, time, xPos, yPos, zPos, commandList, XYZ, timeXYZRecord)
+				
+	curCommandInt, xPos, yPos, zPos, feedRateNumber, measurementUnits = _initialization( curCommandInt, time, xPos, yPos, zPos, commandList, timeXYZRecord)
+	
 	while( curCommandInt < len( commandList ) ):
 		if( commandList[curCommandInt] == ';' ): #Goes through the file to the next commandin the case of a comment
 			while( curCommandInt < len( commandList ) ):
@@ -103,16 +88,14 @@ def parse(filename, output_file=None, graph=False):
 				curCommandInt += 1
 		if( curCommandInt >= len( commandList ) ):
 			break
-		curCommandInt, xPos, yPos, zPos, time = _movementGCode( curCommandInt, time, xPos, yPos, zPos, feedRate, commandList, XYZ, timeXYZRecord)
-
-
-	pprint( timeXYZRecord )
-
-	#Used for plotting the data
-	xValueList = list()
-	yValueList = list()
-	timeList = list()
-
+		curCommandInt, time, xPos, yPos, zPos = _movementGCode( curCommandInt, time, xPos, yPos, zPos, feedRateNumber, commandList, timeXYZRecord)
+	
+	#pprint( timeXYZRecord )	#Prints out the dictionary
+	
+	timeList = []
+	xValueList = []
+	yValueList = []
+	
 	for key in sorted(timeXYZRecord.iterkeys()):
 		timeList.append( key )
 		xValueList.append( timeXYZRecord[key][0] )
@@ -156,4 +139,4 @@ if __name__ == "__main__":
 
 	output_file = len(argv) == 3 and argv[2] or None
 
-	parse(filename, output_file=output_file)
+	parse(filename, output_file=output_file, graph=(output_file is None))
